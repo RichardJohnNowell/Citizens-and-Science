09d2c92206b175c70fd8a3477bfef7f2d0fe7ae3
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-04-06 14:08:24 +0000

    Changed var to let and const

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-var email = document.getElementById('email'),
-    button = document.getElementById('button');
+let email = document.getElementById('email');
+let button = document.getElementById('button');
 
 function validateEmail(email) {
-    var ex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    const ex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return ex.test(email);
 }
 
 email.addEventListener('keydown', function() {
-  var email = this.value;
+  let email = this.value;
   
   if(validateEmail(email)) {
     button.classList.add('is-active');
