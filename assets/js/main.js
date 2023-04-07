39672e5f985c1a549a3a029b1fb0945de1124f84
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-04-07 09:52:47 +0000

    Changed variable names

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,24 @@
-let email = document.getElementById('email');
-let button = document.getElementById('button');
+let emailNews = document.getElementById('email');
+let buttonNews = document.getElementById('button');
 
 function validateEmail(email) {
     const ex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return ex.test(email);
+    return ex.test(emailNews);
 }
 
 email.addEventListener('keydown', function() {
-  let email = this.value;
+  let emailNews = this.value;
   
   if(validateEmail(email)) {
-    button.classList.add('is-active');
+    buttonNews.classList.add('is-active');
   }
 });
 
-button.addEventListener('click', function(e){
+buttonNews.addEventListener('click', function(e){
   e.preventDefault();
   this.classList.add('is-done','is-active');
   
   setTimeout(function(){ 
-    button.innerHTML = "Thanks! Check Your Email."
+    buttonNews.innerHTML = "Thanks! Check Your Email."
   }, 500);
 });
