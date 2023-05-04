e9a7c01e27fd351328c5e4d12a9ee5ca485424bf
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-05-04 09:46:28 +0100

    added comments and text
    changed some text and added or changed comments

--- assets/js/mainAchieve.js
+++ assets/js/mainAchieve.js
@@ -1,5 +1,5 @@
 // start of achievements page quiz
-// an array of read-only questions and answers
+// an array of questions and answers
 const citizenQuestionsAch = [
     { answer: "B", question: "If you wanted to find out about rainfall levels in Ireland? \nA - Deep learning\nB - Millions of Observations\nC - Smartphones and agriculture"},
     { answer: "A", question: "If you wanted to find out about snorkeling in the Indian Ocean? \nA - Involving recreational\nB - Scribes and Scripts\nC - Describing Vocalizations"},
@@ -7,27 +7,27 @@ const citizenQuestionsAch = [
     { answer: "B", question: "If you wanted to find out about Egyptian writing? \nA -  ForestEyes Project\nB - Scribes and Scripts\nC - Describing Vocalizations"},
     { answer: "C", question: "If you wanted to find out about deforestation? \nA - Smartphones and agriculture\nB - Involving recreational\nC - ForestEyes Project"}
 ];
-  // function citizenQuiz and Achievement page
+  //end of array
+  // start of recursive function
   function citizenQuizAch() {
      let score = 0;
       // forEach array method
      citizenQuestionsAch.forEach((question) => {
       // window.prompt method
      let answer = prompt(question.question + "\nAnswer: ");
+       //lowercase to uppercase
        if (answer.toUpperCase() === question.answer) {
        score = score + 1;
        alert("Your answer is correct. Your score is currently " + score);
      } else {
-       alert("Sorry, your answer is not correct.");
+       alert("Your answer is not correct or has invalid input");
      }
    });
      // score accumulator
-    if (score >= 3) {
+    if (score >= 4) {
       alert("Nice one! You have passed " + score);
     } else {
       alert("Sorry, you did not pass " + score);
     }
   }
-     
-  
 //end
