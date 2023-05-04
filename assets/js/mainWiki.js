a63999060039a28305138da4d33115094c6023b4
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-05-04 09:50:28 +0100

    added comments and changed text
    added comments and changed some text. upped pass score.

--- assets/js/mainWiki.js
+++ assets/js/mainWiki.js
@@ -1,5 +1,5 @@
 // start of wikipedia test
-
+// an araay of questions and answers
 const citizenQuestionsWiki = [
 
     { answer: "A", question: "In 1989, who first used the term citizen science? \nA - R. Kerson\nB - Alan Irwin\nC - Muki HaKlay"},
@@ -8,28 +8,28 @@ const citizenQuestionsWiki = [
     { answer: "C", question: "How many breaches of WHO guidelines did wood smoke monitors in Bristol find? \nA - A few\nB - none\nC - 11"},
     { answer: "B", question: "Name the project that helped train AI used by Ellipsis? \nA - The Milky Way project\nB - The Plastic Tide\nC - AnnoTate"}
 ];
-
-  // function citizenQuiz Wikipedia page
+  // end of array
+  // start of recursive function
   function citizenQuizWiki() {
     let score = 0;
      // forEach array method
     citizenQuestionsWiki.forEach((question) => {
      // window.prompt method
     let answer = prompt(question.question + "\nAnswer: ");
+       //lowercase to uppercase
       if (answer.toUpperCase() === question.answer) {
       score = score + 1;
       alert("Your answer is correct. Your score is currently " + score);
     } else {
-      alert("Sorry, your answer is not correct.");
+      alert("Your answer is not correct or has invalid input.");
     }
   });
     // score accumulator
-   if (score >= 3) {
-     alert("Nice one! You have passed " + score);
+   if (score >= 4) {
+     alert("Congratulations! You have passed " + score);
    } else {
      alert("Sorry, you did not pass " + score);
    }
  }
 
- 
 //end
